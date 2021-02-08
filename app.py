5da42906ffc40aeaf2907d22d1a48255489c4669
Abin Singh Rajan <abin.astro@gmail.com>
2021-02-08 12:45:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask 
 from flask_cors import CORS 
 import pymongo 
+import os
 
 # Replace your URL here. Don't forget to replace the password. 
-connection_url = 'mongodb+srv://abin:dbpassword@cluster0.b8byd.mongodb.net/test?retryWrites=true&w=majority'
-
+#connection_url = process.env.MONGODB_URL
+#'mongodb+srv://abin:dbpassword@cluster0.b8byd.mongodb.net/test?retryWrites=true&w=majority'
+connection_url = str(os.environ.get(MONGODB_URL))
 app = Flask(__name__) 
 client = pymongo.MongoClient(connection_url) 
 
